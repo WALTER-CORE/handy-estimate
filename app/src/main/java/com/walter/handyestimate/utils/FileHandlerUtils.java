75005f736a97f0183ea9d02d358fc08391b7200b
Brian Jhong <brianjhong@outlook.com>
2021-05-15 19:12:26 -0700

    Added file handler class and imported apache file lib

---
+++ app/src/main/java/com/walter/handyestimate/utils/FileHandlerUtils.java
@@ -0,0 +1,21 @@
+package com.walter.handyestimate.utils;
+
+import com.walter.handyestimate.data.ContentType;
+
+import java.nio.file.Path;
+import java.util.Map;
+
+public class FileHandlerUtils {
+
+
+    /**
+     * Take a map of strings and format them on a file to print them
+     * accordingly.
+     *
+     * @param input Map of content we should output onto a file.
+     * */
+    public void writeToFile(Map<ContentType, String> input, Path filePath) {
+
+    }
+
+}
---
+++ app/src/test/java/com/walter/handyestimate/utils/FileHandlerUtilsTest.java
@@ -0,0 +1,18 @@
+package com.walter.handyestimate.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class FileHandlerUtilsTest {
+
+    @Test
+    public void testWriteToFile() {
+
+        // Assert file exists.
+        assertTrue();
+        // Assert file contents match.
+        assertEquals();
+    }
+}
