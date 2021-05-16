6cc627fdbc457f9bc6327115ef2d0b878a2af155
JhongHawan <brianjhong@outlook.com>
2021-05-16 00:17:05 -0700

    Add initial FileHandlerUtils

--- app/src/main/java/com/walter/handyestimate/utils/FileHandlerUtils.java
+++ app/src/main/java/com/walter/handyestimate/utils/FileHandlerUtils.java
@@ -2,7 +2,13 @@ package com.walter.handyestimate.utils;
 
 import com.walter.handyestimate.data.ContentType;
 
-import java.nio.file.Path;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileHandlerUtils {
@@ -14,8 +20,38 @@ public class FileHandlerUtils {
      *
      * @param input Map of content we should output onto a file.
      * */
-    public void writeToFile(Map<ContentType, String> input, Path filePath) {
+    public void writeToFile(Map<ContentType, String> input, String filePath) throws IOException {
+        // Create a blank document
+        XWPFDocument xwpfdocument = new XWPFDocument();
 
+        // Create a blank file at C:
+        File file = new File(filePath);
+
+        // Create a file output stream connection
+        try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+            /* Create a new paragraph using the document */
+
+            // CreateParagraph() method is used
+            // to instantiate a new paragraph
+            XWPFParagraph para = xwpfdocument.createParagraph();
+
+            // CreateRun method appends a new run to the
+            // paragraph created
+            XWPFRun xwpfrun = para.createRun();
+
+            // SetText sets the text to the run
+            // created using XWPF run
+            xwpfrun.setText(
+                    "Geeks for Geeks is a computer science portal which aims "
+                            + "to provide all in one platform for learning and "
+                            + "practicing.We can learn multiple programming languages here. ");
+
+            // Write content set using XWPF classes available
+            xwpfdocument.write(outputStream);
+
+            // Close connection
+            outputStream.close();
+        }
     }
 
 }
--- app/src/test/java/com/walter/handyestimate/utils/FileHandlerUtilsTest.java
+++ app/src/test/java/com/walter/handyestimate/utils/FileHandlerUtilsTest.java
@@ -2,17 +2,14 @@ package com.walter.handyestimate.utils;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 public class FileHandlerUtilsTest {
 
     @Test
     public void testWriteToFile() {
 
-        // Assert file exists.
-        assertTrue();
-        // Assert file contents match.
-        assertEquals();
+//        // Assert file exists.
+//        assertTrue();
+//        // Assert file contents match.
+//        assertEquals();
     }
 }
