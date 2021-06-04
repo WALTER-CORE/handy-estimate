9113ec8f03807f901e89c61e32dd887d0ea86d3e
jongjam <jongjam64@gmail.com>
2021-06-03 18:29:06 -0700

    PDFBoxUtilsTest made, writes PDF to direcetory

--- app/src/main/java/com/walter/handyestimate/utils/PDFFileUtils.java
+++ app/src/main/java/com/walter/handyestimate/utils/PDFFileUtils.java
@@ -17,6 +17,14 @@ import java.io.IOException;
 
 public class PDFFileUtils {
 
+    public static final int HEADING_FONT_SIZE = 20;
+    public static final int HEADING_SPACING_AFTER = 500;
+    public static final int PARAGRAPH_FONT_SIZE = 12;
+    public static final int DEFAULT_SPACING_AFTER = 1000;
+    private static int currentYOffset;
+
+
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static void writeEstimateToPDF(Estimate estimate, String filePath) throws IOException {
         FileOutputStream stream = new FileOutputStream(filePath); //need to make file in activity first to get filepathContentStreamWriter csw = new ContentStreamWriter(stream);
@@ -26,19 +34,34 @@ public class PDFFileUtils {
 
 //        PdfDocument.Page page1 = myPdf.startPage(myPdfInfo);
         PDPage page = new PDPage();
-
+        currentYOffset = 500;
 //        Canvas canvas = page1.getCanvas()
-
+        //8.5 x 11 inch = 2550 x 3300 pixel
         //code to insert text and table
+        myPdf.addPage(page);
         PDPageContentStream contentStream = new PDPageContentStream(myPdf, page);
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-        contentStream.moveTo(100, 700);
-        contentStream.showText(estimate.getCompanyAddress());
-        contentStream.endText();
-
+        writeEstimateHeading(contentStream);
+        writeEstimateCompanyAddress(contentStream, estimate.getCompanyAddress());
+        contentStream.close();
         myPdf.save(stream);
         stream.close();
         myPdf.close();
     }
+
+    public static void writeEstimateHeading(PDPageContentStream contentStream) throws IOException {
+        contentStream.beginText();
+        contentStream.newLineAtOffset(5, currentYOffset);
+        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 20);
+        contentStream.showText("Handy Estimate");
+        contentStream.endText();
+    }
+
+    public static void writeEstimateCompanyAddress(PDPageContentStream contentStream, String companyAddress) throws IOException {
+        contentStream.beginText();
+        contentStream.newLineAtOffset(5, currentYOffset - DEFAULT_SPACING_AFTER);
+        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+        contentStream.showText(companyAddress);
+        contentStream.endText();
+    }
+
 }
--- app/src/test/java/com/walter/handyestimate/utils/PDFFileUtilsTest.java
+++ app/src/test/java/com/walter/handyestimate/utils/PDFFileUtilsTest.java
@@ -1,4 +1,42 @@
 package com.walter.handyestimate.utils;
 
-public class PDFFileUtilsTest {
+import com.walter.handyestimate.data.model.Estimate;
+import com.walter.handyestimate.data.model.EstimateLineItem;
+import com.walter.handyestimate.data.model.EstimateTable;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static com.walter.handyestimate.utils.PDFFileUtils.writeEstimateToPDF;
+
+public class PDFFileUtilsTest extends TestCase {
+
+    public void testWriteEstimateToPDF() throws IOException {
+
+        String path = "C:\\Users\\jongj\\OneDrive\\Desktop\\temp\\pdf.pdf";
+        File pdf = new File(path);
+
+        String estimateDescription = "[ESTIMATE DESCRIPTION] Estimate for fence repair";
+        String companyName = "[COMPANY NAME] Handy Company";
+        String companyAddress = "[COMPANY ADDRESS] 1234 Sesame Street, Seattle WA 11111";
+        String customerName = "[CUSTOMER NAME] Customer Name";
+        String customerAddress = "[COMPANY ADDRESS] 5678 Customer Way, Seattle WA 22222";
+        EstimateTable estimateTable = new EstimateTable(
+                Arrays.asList(
+                        new EstimateLineItem("Item 1", 1, BigDecimal.TEN),
+                        new EstimateLineItem("Item 2", 2, BigDecimal.valueOf(20))
+                )
+        );
+        Estimate estimate = new Estimate(estimateDescription, companyName, companyAddress, customerName,
+                customerAddress, estimateTable);
+
+        writeEstimateToPDF(estimate, path);
+
+        assertNotNull(pdf);
+    }
+
 }
