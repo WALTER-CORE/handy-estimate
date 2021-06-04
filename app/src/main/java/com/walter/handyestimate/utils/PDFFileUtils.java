217b9e3d17a7555d904d884995d8caf6c9d3a584
jongjam <jongjam64@gmail.com>
2021-06-04 00:38:46 -0700

    completed template with table

--- app/src/main/java/com/walter/handyestimate/utils/PDFFileUtils.java
+++ app/src/main/java/com/walter/handyestimate/utils/PDFFileUtils.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import androidx.annotation.RequiresApi;
 
 import com.walter.handyestimate.data.model.Estimate;
+import com.walter.handyestimate.data.model.EstimateLineItem;
 import com.walter.handyestimate.data.model.EstimateTable;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -16,6 +17,7 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public class PDFFileUtils {
@@ -31,16 +33,11 @@ public class PDFFileUtils {
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static void writeEstimateToPDF(Estimate estimate, String filePath) throws IOException {
         FileOutputStream stream = new FileOutputStream(filePath); //need to make file in activity first to get filepathContentStreamWriter csw = new ContentStreamWriter(stream);
-//        PdfDocument.PageInfo.Builder builder = new PdfDocument.PageInfo.Builder(2550, 3000, 1);
-//        PdfDocument.PageInfo myPdfInfo = builder.create();
         PDDocument myPdf = new PDDocument();
-
-//        PdfDocument.Page page1 = myPdf.startPage(myPdfInfo);
         PDPage page = new PDPage();
+
         currentYOffset = 750;
-//        Canvas canvas = page1.getCanvas()
-        //8.5 x 11 inch = 2550 x 3300 pixel
-        //code to insert text and table
+
         myPdf.addPage(page);
         PDPageContentStream contentStream = new PDPageContentStream(myPdf, page);
         writeEstimateHeading(contentStream);
@@ -50,6 +47,7 @@ public class PDFFileUtils {
         writeEstimateCompanyInfo(contentStream,estimate.getCompanyName(), estimate.getCompanyAddress());
         writeCustomerInfo(contentStream, estimate.getCustomerName(), estimate.getCustomerAddress());
         writeEstimateTable(contentStream, estimate.getEstimateTable());
+        drawEstimateTable(contentStream, page, estimate.getEstimateTable(), currentYOffset - 100, 30 );
         contentStream.close();
         myPdf.save(stream);
         stream.close();
@@ -140,7 +138,151 @@ public class PDFFileUtils {
     public static void writeEstimateTable(PDPageContentStream contentStream, EstimateTable estimateTable) throws IOException {
 
     }
+    //This method draws the table
+    //Perhaps put contents of estimateTable into the 2d array
+    //y is coordinate start
+    public static void drawEstimateTable(PDPageContentStream contentStream, PDPage page, EstimateTable estimateTable,
+                                         float yPosition, float margin) throws IOException {
+        final int rows = estimateTable.getEstimateLineItemList().size() + 1;
+        final int cols = 4;
+        final float rowHeight = 20f;
+        final float tableWidth = page.getMediaBox().getWidth()-(2*margin);
+        final float tableHeight = rowHeight * rows;
+        final float colWidth = tableWidth/(float)cols;
+        final float cellMargin=5f;
+        List<EstimateLineItem> lineItems = estimateTable.getEstimateLineItemList();
+        //4 rows always
+        //cols is EstimateLineItem list length
+
+        //draw title row
+        float nexty = yPosition ;
+        String[] titles = {"Description", "Quantity", "Rate", "Cost"};
+//        for (int i = 0; i <= rows; i++) {
+        //draw the rows
+
+        for (int i = 0; i <= rows; i++) {
+            contentStream.moveTo(margin,nexty);
+            contentStream.lineTo(margin+tableWidth,nexty);
+            contentStream.stroke();
+            nexty-= rowHeight;
+        }
+
+        //draw the columns
+        float nextx = margin;
+        for (int i = 0; i <= cols; i++) {
+            contentStream.moveTo(nextx, yPosition);
+            contentStream.lineTo(nextx,yPosition-tableHeight);
+            contentStream.stroke();
+            nextx += colWidth;
+        }
 
 
 
+        float textx = margin+cellMargin;
+        float texty = yPosition-15;
+
+        //now add the text
+        contentStream.setFont(PDType1Font.HELVETICA_BOLD,14);
+
+        //This loop prints out just the titles
+        for(int i = 0; i < titles.length; i++){
+                String text = titles[i];
+                contentStream.beginText();
+                contentStream.newLineAtOffset(textx,texty);
+                contentStream.showText(text);
+                contentStream.endText();
+                textx += colWidth;
+        }
+        texty-=rowHeight;
+        textx = margin+cellMargin;
+
+        contentStream.setFont(PDType1Font.HELVETICA,12);
+        for(int i = 0; i < lineItems.size(); i++){
+            for(int j = 1 ; j <= 4; j++){ //1 to 3 to get different fields
+                String text;
+                if (i == 1) {
+                    //Description
+                    text = lineItems.get(i).getDescription();
+                } else if (i == 2) {
+                    //Quantity
+                    text = Integer.toString(lineItems.get(i).getQuantity());
+                } else if (i == 3) {
+                    //Rate
+                    text = lineItems.get(i).getRate().toString();
+                } else {
+                    //Cost
+                    text = lineItems.get(i).getCost().toString();
+                }
+
+                contentStream.beginText();
+                contentStream.newLineAtOffset(textx,texty);
+                contentStream.showText(text);
+                contentStream.endText();
+
+                textx += colWidth;
+            }
+            texty-=rowHeight;
+            textx = margin+cellMargin;
+        }
+
+//        for(int j = 1 ; j <= 3; j++){ //This loop will just have to be hard coded
+//            String text = content[i][j];
+//            contentStream.beginText();
+//            contentStream.newLineAtOffset(textx,texty);
+//            contentStream.showText(text);
+//            contentStream.endText();
+//            textx += colWidth;
+//        }
+
+        contentStream.setFont(PDType1Font.HELVETICA_BOLD,14);
+        contentStream.beginText();
+        contentStream.newLineAtOffset(430, texty - 50);
+        contentStream.showText("Total Cost : " + estimateTable.getTotalCost());
+        contentStream.endText();
+    }
+
+    //ORIGINAL CODE
+    //public static void drawTable(PDPage page, PDPageContentStream contentStream,
+    //                            float y, float margin,
+    //                            String[][] content) throws IOException {
+    //    final int rows = content.length;
+    //    final int cols = content[0].length;
+    //    final float rowHeight = 20f;
+    //    final float tableWidth = page.findMediaBox().getWidth()-(2*margin);
+    //    final float tableHeight = rowHeight * rows;
+    //    final float colWidth = tableWidth/(float)cols;
+    //    final float cellMargin=5f;
+    //
+    //    //draw the rows
+    //    float nexty = y ;
+    //    for (int i = 0; i <= rows; i++) {
+    //        contentStream.drawLine(margin,nexty,margin+tableWidth,nexty);
+    //        nexty-= rowHeight;
+    //    }
+    //
+    //    //draw the columns
+    //    float nextx = margin;
+    //    for (int i = 0; i <= cols; i++) {
+    //        contentStream.drawLine(nextx,y,nextx,y-tableHeight);
+    //        nextx += colWidth;
+    //    }
+    //
+    //    //now add the text
+    //    contentStream.setFont(PDType1Font.HELVETICA_BOLD,12);
+    //
+    //    float textx = margin+cellMargin;
+    //    float texty = y-15;
+    //    for(int i = 0; i < content.length; i++){
+    //        for(int j = 0 ; j < content[i].length; j++){
+    //            String text = content[i][j];
+    //            contentStream.beginText();
+    //            contentStream.moveTextPositionByAmount(textx,texty);
+    //            contentStream.drawString(text);
+    //            contentStream.endText();
+    //            textx += colWidth;
+    //        }
+    //        texty-=rowHeight;
+    //        textx = margin+cellMargin;
+    //    }
+    //}
 }
